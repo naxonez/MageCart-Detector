124e0a30a02792e75a1b8cba0d3c473b3ee16a02
naxonez <naxonez@gmail.com>
2018-09-22 01:06:55 +0200

    Create content.js

---
+++ content.js
@@ -0,0 +1,36 @@
+var scripts = document.querySelectorAll('script[src]');
+
+function detectPattern(payload,link)
+{
+	var re = /var _0x[a-z]{1}[0-9]{3}=\["\\x[A-F0-9]{2}\\/gm;
+	if( payload.match(re) ){
+		alert("Possible Magecart found - "+link);
+		return 1;
+	}else{
+		return 0;
+	}
+
+}
+
+function httpGet(theUrl)
+{
+	var req = new XMLHttpRequest();
+	req.open('GET', theUrl, true);
+	req.onreadystatechange = function (aEvt) {
+  	if (req.readyState == 4) {
+     		if(req.status == 200){
+      			detectPattern(req.responseText, theUrl);
+		}
+	}
+	};
+	req.send(null);
+}
+
+function main()
+{
+	for (var i = 0; i < scripts.length; ++i) {
+		httpGet(scripts[i].getAttribute("src"));
+	}
+}
+
+main();
